91c45a250e47b870102aa6a32fd3a10426bbba74
falloutx <inderjitsidhu@outlook.com>
2015-06-26 23:46:35 +0530

    Add Command line Interface. Add Simple Coloring

--- todo.py
+++ todo.py
@@ -1,14 +1,17 @@
 from firebase import firebase
+from clint.textui import puts, indent, colored
+import argparse
+
 import json
 
 todobase = firebase.FirebaseApplication('https://shining-heat-5315.firebaseio.com/', None)
 
+# TODO: Better Modelling for todos.
 def get_todos():
     todos = todobase.get('/todos', None)
     for key in todos.keys():
         todo = todos[key]
-        # print todo
-        print todo['task'] + " : " + str(todo['bounty'])
+        puts( colored.red(str(todo['bounty'])) + " : " + todo['task'] )
 
 
 def add_todo(task, points=1):
@@ -21,4 +24,25 @@ def add_todo(task, points=1):
 # add_todo('Write Up A Blog Post', 15)
 # add_todo('Set Up Private Eye Repo', 10)
 
-get_todos()
+#get_todos()
+def main():
+    parser = argparse.ArgumentParser(description='A Todo List Manager in your command line')
+    # get argument
+    parser.add_argument('--get', nargs='?', metavar="task")
+    # add todo argument
+    parser.add_argument('--add', nargs=2, metavar=('task', 'bounty'))
+
+    args = parser.parse_args()
+    if args.add and len(args.add) == 2:
+        task = str(args.add[0]) # task
+        bounty = int(args.add[1])
+        add_todo(task, bounty)
+
+    if args.get == None:
+        get_todos()
+
+
+
+
+if __name__ == '__main__':
+    main()
