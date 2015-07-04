11047f11c4f749413f67f5c8b4f2d2c4195193cc
falloutx <inderjitsidhu@outlook.com>
2015-07-04 16:29:48 +0530

    Add Simple Progress Bar.

--- lib/userprofile.py
+++ lib/userprofile.py
@@ -1,4 +1,4 @@
-from clint.textui import puts, indent, colored
+from clint.textui import puts, indent, colored, progress
 
 class Profile(object):
 
@@ -8,6 +8,7 @@ class Profile(object):
         self._profile = self._base.get(self._url, 'me')
         self._todos = todos.fetch_todos()
         self._rewards = rewards.fetch_rewards()
+        self._progress_bar_size = 40
 
     def update(self):
         total_points, earned_points, total_items, items_done, rewards_redeemed = self._calculate_points()
@@ -28,9 +29,8 @@ class Profile(object):
         items_done = 0
         total_items = 0
         rewards_redeemed = 0
-        for folder in self._todos.keys()
 
-            for key in folder.keys():
+        for key in self._todos.keys():
             todo = self._todos[key]
             total_points += todo['bounty']
             earned_points += todo['bounty'] if todo['done'] else 0
@@ -53,7 +53,18 @@ class Profile(object):
 
         puts ( 'Total Items: ' +  colored.blue(str(self._profile['total_items'])))
         puts ( 'Total Done: ' +  colored.green(str(self._profile['done'])))
+        percent_done = (self._profile['done'] / float(self._profile['total_items']))*100
+        puts (self._print_progress_bar('Done', percent_done))
         puts ( 'Total Points: ' +  colored.blue(str(self._profile['available'])))
 
         puts ( 'Points Earned: ' +  colored.green(str(self._profile['points'])))
+        percent_points = (self._profile['points'] / float(self._profile['available']))*100
+        puts ( self._print_progress_bar('Points gained', percent_points ))
         puts ( 'Rewards Redeemed: ' +  colored.green(str(self._profile['rewards_redeemed'])))
+
+    def _print_progress_bar(self, label, percent, ex_size=100):
+        size = self._progress_bar_size
+        scale = float(size)/ex_size
+        bar_size = int(scale*percent)
+        bar = '#'* bar_size + ">" + ' '*(size-bar_size-1) if bar_size < size-1 else '#'*bar_size
+        return label + ': ['+ colored.green(bar)  +']'
